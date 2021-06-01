00658404d97d8af9c00937ac05c42f8cb6d4f190
jgambox <ing.juancgamboa@gmail.com>
2021-06-01 00:29:29 -0500

    [UPDATE] Challenge AVIANCA files added

---
+++ dwfs-online-acce-1/SPRING_2 JavaScript/WEEK_11 Estructura aplicacion y Arrays/MEETING_21 Estructura de un programa/Challenge/main.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 10; i++) {
+    console.log("For loop print 1-10: vuelta " + i);
+}
+
+n = 0;
+
+while (n < 10) {
+    n ++;
+    console.log("While loop print 1-10: vuelta " + n);
+}
